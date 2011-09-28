46ed83b616e2f62e5af32672a52caad0ca0a5eb3
Martin Nemitz <martin.nemitz@compori.com>
2011-09-28 19:20:05 +0200

    Create a close button

--- HelloGitHubWindowsApplication/MainForm.Designer.vb
+++ HelloGitHubWindowsApplication/MainForm.Designer.vb
@@ -23,6 +23,7 @@ Partial Class MainForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.CloseButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -34,11 +35,21 @@ Partial Class MainForm
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Hello World!"
         '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(388, 251)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(75, 23)
+        Me.CloseButton.TabIndex = 1
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(475, 286)
+        Me.Controls.Add(Me.CloseButton)
         Me.Controls.Add(Me.Label1)
         Me.Name = "MainForm"
         Me.Text = "Testing Playground with Git"
@@ -47,5 +58,6 @@ Partial Class MainForm
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CloseButton As System.Windows.Forms.Button
 
 End Class
--- HelloGitHubWindowsApplication/MainForm.vb
+++ HelloGitHubWindowsApplication/MainForm.vb
@@ -1,3 +1,7 @@
 ﻿Public Class MainForm
 
+    Private Sub CloseButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseButton.Click
+        Me.Close()
+
+    End Sub
 End Class
